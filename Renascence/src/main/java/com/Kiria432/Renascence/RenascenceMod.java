941a1eed13813a7c5258aad4eba9f8a38c3f796e
Kiria432 <kaizerlucifer43@gmail.com>
2025-09-05 13:21:38 +0500

    Add files via upload

---
+++ Renascence/src/main/java/com/Kiria432/Renascence/RenascenceMod.java
@@ -0,0 +1,12 @@
+package com.Kiria432.Renascence;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RenascenceMod.MODID)
+public class RenascenceMod {
+    public static final String MODID = "renascence";
+    
+    public RenascenceMod() {
+        // Базовая инициализация мода
+    }
+}
